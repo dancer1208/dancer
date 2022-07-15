8ba59bc57b4be4df8e909aacfdc927088e650505
dancer1208 <dancer@stu.edu.tw>
2022-07-15 16:41:42 +0800

    v4

---
+++ 0715/js/js4.js
@@ -0,0 +1,12 @@
+var score = prompt("請輸入考試成績：")
+
+if (score >= 90)
+alert("您獲得的成績是 A ！")
+else if (score >= 80 & score < 90)
+alert("您獲得的成績是 B ！")
+else if (score >= 70 & score < 80)
+alert("您獲得的成績是 C ！")
+else if (score >= 60 & score < 70)
+alert("您獲得的成績是 D ！")
+else
+alert("您獲得的成績是 F ！")
